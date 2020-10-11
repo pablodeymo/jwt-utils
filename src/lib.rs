4ddcd890ef3f21ecc5380d8d8ea1bab59857c1c2
Pablo Deymonnaz <deymonnaz@gmail.com>
2020-10-10 21:13:15 -0300

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+use anyhow::{anyhow, Result};
+use chrono::prelude::*;
+use jsonwebtoken::{encode, EncodingKey, Header};
+use serde::{Deserialize, Serialize};
+
+pub fn calculate_jwt_with_username(username: &str, secret: &[u8]) -> Result<String> {
+    #[derive(Debug, Serialize, Deserialize)]
+    struct Claims<'a> {
+        iss: &'a str,
+        iat: i64,
+    }
+
+    let local: DateTime<Local> = Local::now();
+
+    let jwt_claim = Claims {
+        iss: username,
+        iat: local.timestamp(),
+    };
+
+    // jwt_claim is a struct that implements Serialize
+    // This will create a JWT using HS256 as algorithm
+    encode(
+        &Header::default(),
+        &jwt_claim,
+        &EncodingKey::from_secret(secret),
+    )
+    .map_err(|e| anyhow!("Error generating JWT. {}", e))
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
