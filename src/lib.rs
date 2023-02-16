9708cdd0e55c51a9af3ef03af3f1f2ac6fa3891f
Pablo Deymonnaz <pdeymon@fi.uba.ar>
2023-02-15 23:29:12 -0300

    changes and upgrades

--- src/lib.rs
+++ src/lib.rs
@@ -11,15 +11,20 @@ struct Claims<'a> {
     exp: usize,
 }
 
-pub fn encode_jwt_with_username(userid: &str, username: &str, secret: &[u8]) -> Result<String> {
-    let local: DateTime<Local> = Local::now();
+impl<'a> Claims<'a> {
+    pub fn new(userid: &'a str, username: &'a str, exp: usize) -> Claims<'a> {
+        let local: DateTime<Local> = Local::now();
+        Claims {
+            sub: userid,
+            name: username,
+            iat: local.timestamp(),
+            exp,
+        }
+    }
+}
 
-    let jwt_claim = Claims {
-        sub: userid,
-        name: username,
-        iat: local.timestamp(),
-        exp: 10000000000,
-    };
+pub fn encode_jwt_with_username(userid: &str, username: &str, secret: &[u8]) -> Result<String> {
+    let jwt_claim = Claims::new(userid, username, 10_000_000_000);
 
     // jwt_claim is a struct that implements Serialize
     // This will create a JWT using HS256 as algorithm
@@ -28,7 +33,7 @@ pub fn encode_jwt_with_username(userid: &str, username: &str, secret: &[u8]) -> 
         &jwt_claim,
         &EncodingKey::from_secret(secret),
     )
-    .map_err(|e| anyhow!("Error generating JWT. {}", e))
+    .map_err(|e| anyhow!("Error generating JWT. {e}"))
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -38,13 +43,15 @@ pub struct DeserializedClaims {
     pub iat: i64,
 }
 
-pub fn decode_jwt_token(token: &str, secret: &[u8]) -> Result<DeserializedClaims> {
-    // Claims is a struct that implements Deserialize
-    decode::<DeserializedClaims>(
-        &token,
-        &DecodingKey::from_secret(secret),
-        &Validation::new(Algorithm::HS256),
-    )
-    .map(|v| v.claims)
-    .map_err(|e| anyhow!("Error decoding JWT token. {}", e))
+impl DeserializedClaims {
+    pub fn decode_jwt_token(token: &str, secret: &[u8]) -> Result<Self> {
+        // Claims is a struct that implements Deserialize
+        decode::<DeserializedClaims>(
+            token,
+            &DecodingKey::from_secret(secret),
+            &Validation::new(Algorithm::HS256),
+        )
+        .map(|v| v.claims)
+        .map_err(|e| anyhow!("Error decoding JWT token. {e}"))
+    }
 }
